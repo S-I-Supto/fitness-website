4e19cb7febc1f34f4001fe748708d1de2b59fbfe
Supto <110963939+S-I-Supto@users.noreply.github.com>
2023-05-15 16:55:46 +0600

    changed fonts and compressed image of hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -12,7 +12,9 @@ const HeroSection = () => {
         color: 'white',
         borderRadius: '2px',
         fontSize: { xs: '17px', lg: '22px' },
-        fontFamily: 'TT-medium-italic',
+        fontStyle: "italic",
+        fontWeight: "500",
+        fontFamily: 'Montserrat',
         letterSpacing: '1.1px',
         "&:hover": {
             boxShadow: 8,
@@ -21,12 +23,12 @@ const HeroSection = () => {
         },
     };
     return (
-        <Box component={motion.div} variants={Parent} initial='hidden' animate='show' sx={{ width: '100vw', height: '67vh', marginTop: '220px', padding: { md: '0px 30px', xs: '0px 0px', } }}>
+        <Box component={motion.div} variants={Parent} initial='hidden' animate='show' sx={{ width: '100vw', height: '67vh', marginTop: '220px', padding: { md: '0px 30px', xs: '0px 13px', } }}>
 
-            <Typography component={motion.p} variants={leftStyle} sx={{ fontFamily: 'TT-bold-italic', fontSize: { md: '39px', xs: '30px' }, color: 'white', lineHeight: '161%', letterSpacing: '0.05em', fontWeight: 700, marginBottom: '21px', textAlign: { xs: 'center', md: 'left' } }}>
+            <Typography component={motion.p} variants={leftStyle} sx={{ fontFamily: 'Montserrat', fontStyle: "italic", fontSize: { md: '39px', xs: '28px' }, color: 'white', lineHeight: '161%', letterSpacing: '0.05em', fontWeight: 700, marginBottom: '21px', textAlign: { xs: 'center', md: 'left' } }}>
                 Get <span className='red-text'>Fit</span>  and <span className='red-text'>Feel Great</span>  with  Our <br /> <span className='red-text'>Customized Workout Plans.</span>
             </Typography>
-            <Typography component={motion.p} variants={leftStyle} className='Cera-light' sx={{ fontSize: { xs: '14px', md: '17px' }, letterSpacing: '0.1em', color: 'white', textAlign: { xs: 'center', md: 'left' }, marginBottom: '36px' }}>Get fit on your own terms with our on-demand workout. </Typography>
+            <Typography component={motion.p} variants={leftStyle} sx={{ fontSize: { xs: '14px', md: '17px' }, fontFamily: "Poppins", fontWeight: "400", letterSpacing: '0.1em', color: 'white', textAlign: { xs: 'center', md: 'left' }, marginBottom: '36px' }}>Get fit on your own terms with our on-demand workout. </Typography>
             <Box sx={{ display: 'flex', justifyContent: { xs: 'center', md: 'flex-start' } }}>
                 <Button component={motion.div} variants={leftStyle} sx={styles}>
                     Join now
